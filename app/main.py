3311a6b6bc4adf5890ea63526a0e4b33464c1386
RRoggia <renanzr@gmail.com>
2025-02-04 15:49:52 -0300

    add init py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from app.routers.v1.example_2 import example_router2
-from app.routers.v1.example_router import example_router
+from app.routers.v1 import example_2
+from app.routers.v1 import example_router
+
+V1 = "/v1"
 
 app = FastAPI()
-app.include_router(example_router, prefix="/v1")
-app.include_router(example_router2, prefix="/v1")
+app.include_router(example_router.example_router, prefix=V1)
+app.include_router(example_2.example_router2, prefix=V1)
 
 
 @app.get("/")
